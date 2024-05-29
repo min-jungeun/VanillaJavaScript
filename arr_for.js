9e01791c7e6f35131fad3176fa03ba0ecff212a4
min-jungeun <mje971115@gmail.com>
2024-05-29 12:40:26 +0900

    후치연산, 전치연산 계산 법

--- arr_for.js
+++ arr_for.js
@@ -1,16 +1,20 @@
-const nav_data = [
-    {
-        gnb_li_a : "회사소개",
-        gnb_li_href : "./company.html"
-    },
-    {
-        gnb_li_a : "제품소개",
-        gnb_li_href : "./product.html"
-    },
-    {
-        gnb_li_a : "커뮤니티",
-        gnb_li_href : "./board.html"
-    }
+console.log("한번만 실행되지");
 
-]
-// console.log(nav_data[2].gnb_li_a)
+// 초기값을 읽고 조건을 읽고 참이라면 실행하고 연산식실행, 아직도 참이라면 실행하고 연산식실행
+// 그러다가 조건이 거짓이 되는 순간 for문 종료
+
+for(var i = 0; i < 5; i++ ){
+console.log("나는 다섯번 실행되지");
+}
+
+let count = 10;
+    count += 100; //110
+    count += 1; //111
+    ++count; //112
+    count++; //112
+
+console.log(count); //113
+// count++;  //count += 1 1씩 증가하겠다는 뜻(추가 저장)->후치연산,  ++count -> 전치연산
+// count++; //10
+// count; //11
+// ++count; //12
