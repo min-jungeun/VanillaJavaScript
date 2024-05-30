dcb325d2c480536f4c5302463c63b863a39e850b
min-jungeun <mje971115@gmail.com>
2024-05-30 12:48:41 +0900

    for_for문

---
+++ for_for.js
@@ -0,0 +1,10 @@
+let dayoutput = "";
+
+for(let joonm =0; joonm < 4; joonm++){
+    for(let daynm = 1; daynm < 8; daynm++){
+
+        dayoutput += `${daynm}\t`;
+        
+    }
+}
+console.log(dayoutput)
